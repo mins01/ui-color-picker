bcc5be9ab3394cefddddb74f033dde5dad5274f3
공대여자 <mins01.lycos.co.kr@gmail.com>
2026-03-22 15:43:00 +0900

    src/ui-color-picker.js

---
+++ src/ui-color-picker.js
@@ -0,0 +1,16 @@
+import UiColorElement from './UiColorElement.js';
+UiColorElement.defineCustomElement();
+import UiColorBarHueElement from './UiColorBarHueElement.js';
+UiColorBarHueElement.defineCustomElement();
+import UiColorWheelHueElement from './UiColorWheelHueElement.js';
+UiColorWheelHueElement.defineCustomElement();
+import UiColorPlaneSlElement from './UiColorPlaneSlElement.js';
+UiColorPlaneSlElement.defineCustomElement();
+import UiColorPlaneSbElement from './UiColorPlaneSbElement.js';
+UiColorPlaneSbElement.defineCustomElement();
+import UiColorSwatchElement from './UiColorSwatchElement.js';
+UiColorSwatchElement.defineCustomElement();
+import UiColorSwatchesElement from './UiColorSwatchesElement.js';
+UiColorSwatchesElement.defineCustomElement();
+import UiColorPickerElement from './UiColorPickerElement.js';
+UiColorPickerElement.defineCustomElement();
